f03393649d63bdbeb2306223571d0211c7bc80cd
JuanvSequeira <juanvictor.sequeira@gmail.com>
2025-03-18 11:19:51 -0400

    salvando estudo

---
+++ Java-Anatomia-Classe/src/edu/juan/segunda semana/Estudodevariaveis.java
@@ -0,0 +1,13 @@
+
+public class Estudodevariaveis {
+  public static void main(String[] args) {
+    String meuNome = "Juan";
+    Float pi = 3.14F;
+    Long cpfLong = 99850413748L;
+    double salarioMinimo = 2500;
+    short numeroCurto = 1;
+    int numeroNormal = numeroCurto;
+    short numeroCurto2 = (short)numeroNormal;
+    final double NUMERO_PI = 3.14;
+  }
+}
