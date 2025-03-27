08aec56141c35a80c0e2ffef7a4ff5fa74a8f659
juan victor sequeira santos <121132342+JuanvSequeira@users.noreply.github.com>
2025-03-27 13:39:21 -0400

    Condicionaiscompostas atualizado

---
+++ Controle de Fluxo (Condicionais)/Condicionais/src/Condicionaiscomposta.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class Condicionaiscomposta {
+
+ int nota = 0;
+ int media = 7;
+
+    public static void main(String[] args) {
+      Condicionaiscomposta cNotas = new Condicionaiscomposta();
+      Scanner scan = new Scanner(System.in);
+      
+      System.out.println("Digite a sua nota : ");
+      cNotas.nota = scan.nextInt();
+
+     
+      if (cNotas.nota >= cNotas.media) {
+        
+        System.out.println("Aprovado com sucesso : "+ cNotas.nota);
+      }else if (cNotas.nota >= 5 && cNotas.media  < 7){
+        System.out.println("Prova de recuperação");
+      }else if (cNotas.nota > 5 && cNotas.nota  < 7){
+        System.out.println("Prova de recuperação");
+      }else{
+        System.out.println("Reprovado "+cNotas.nota);
+      }
+
+
+    }
+}
